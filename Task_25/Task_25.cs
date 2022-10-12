78d735ca3e2da505a18890b129701e5659360087
Vyachaslav75 <kslava@tut.by>
2022-10-12 20:42:46 +0300

    Add Task 25

---
+++ Task_25/Task_25.cs
@@ -0,0 +1,17 @@
+﻿int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+Console.Clear();
+int A = enterNumber("Введите число А:  ", "Введите целое число");
+int B = enterNumber("Введите число В:  ", "Введите целое число");
+Console.WriteLine($"{A}, {B} -> {Math.Pow(A, B)}");
