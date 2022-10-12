a25d1bff9dafec82f78e01f06de3e2576dbf9622
Vyachaslav75 <kslava@tut.by>
2022-10-12 20:44:55 +0300

    Add Task 27

---
+++ Task_27/Task_27.cs
@@ -0,0 +1,30 @@
+﻿int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+int sumNum(int num)
+{
+    //int count = 0;
+    int sum = 0;
+    //if (num == 0) return count = 1;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+        //count++;
+    }
+    return sum;
+}
+
+Console.Clear();
+int userNumber = enterNumber("Введите целое число:  ", "Введите целое число");
+Console.WriteLine($"{userNumber} -> {sumNum(userNumber)}");
